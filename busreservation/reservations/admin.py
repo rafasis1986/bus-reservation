0836b075c8383dead8d171d62c436b9de162ddb0
Rafael Torres <rdtr.sis@gmail.com>
2019-03-11 01:34:52 -0300

    Revert "merge front with backend"

--- busreservation/reservations/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from busreservation.reservations import models
-
-admin.site.register(models.Bus)
-admin.site.register(models.City)
-admin.site.register(models.Itinerary)
-admin.site.register(models.Ticket)
-admin.site.register(models.Trip)
